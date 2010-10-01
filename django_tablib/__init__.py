915356824ac866239554720894eca8de5b090f45
Josh Ourisman <josh@joshourisman.com>
2010-09-30 20:52:12 -0400

    now allowing user to specify headers

--- django_tablib/__init__.py
+++ django_tablib/__init__.py
@@ -5,9 +5,12 @@ from django.template.defaultfilters import date
 from django.utils.encoding import smart_unicode
 
 class Dataset(tablib.Dataset):
-    def __init__(self, queryset):
-        fields = queryset.model._meta.fields
-        self.headers_list = [field.name for field in fields]
+    def __init__(self, queryset, headers=None):
+        if headers is None:
+            fields = queryset.model._meta.fields
+            self.headers_list = [field.name for field in fields]
+        else:
+            self.headers_list = headers
         data = map(self._getattrs, queryset)
         super(Dataset, self).__init__(headers=self.headers_list, *data)
 
