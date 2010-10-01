89eaaa02539af8ae59ab610a9a0e83ae84a3821c
Josh Ourisman <josh@joshourisman.com>
2010-09-30 20:49:09 -0400

    added basic Dataset model that works with querysets

---
+++ django_tablib/__init__.py
@@ -0,0 +1,31 @@
+import datetime
+import tablib
+
+from django.template.defaultfilters import date
+from django.utils.encoding import smart_unicode
+
+class Dataset(tablib.Dataset):
+    def __init__(self, queryset):
+        fields = queryset.model._meta.fields
+        self.headers_list = [field.name for field in fields]
+        data = map(self._getattrs, queryset)
+        super(Dataset, self).__init__(headers=self.headers_list, *data)
+
+    def _cleanval(self, value, attr):
+        if callable(value):
+            value = value()
+        t = type(value)
+        if t is str:
+            return value
+        elif t in [datetime.date, datetime.datetime]:
+            return date(value, 'SHORT_DATE_FORMAT')
+        else:
+            return smart_unicode(value).encode('ascii', 'xmlcharrefreplace')
+    
+    def _getattrs(self, obj):
+        attrs = []
+        for attr in self.headers_list:
+            attr = self._cleanval(getattr(obj, attr), attr)
+            attrs.append(attr)
+        return attrs
+        
