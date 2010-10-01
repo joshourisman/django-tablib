6d77f8466b6a1818bf2e401fbae6babeb747fa5c
Josh Ourisman <josh@joshourisman.com>
2010-09-30 21:25:47 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+description = "A wrapper around Kenneth Reitz' tablib to work with Django models."
+
+VERSION = '1.0'
+
+setup(
+    name='django-tablib',
+    version=VERSION,
+    author='Joshua Ourisman',
+    author_email='josh@joshourisman.com',
+    url='http://bitbucket.org/Josh/django-tablib',
+    description=description,
+    long_description=description,
+    license='MIT',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+    packages=['django_tablib',],
+    install_requires=['django', 'tablib',],
+    )
