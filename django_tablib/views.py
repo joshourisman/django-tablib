fcd15a6ed4640e579bc6c7bdf9ae63b8f58f7c06
Josh Ourisman <josh@joshourisman.com>
2010-10-05 11:19:05 -0400

    basic export to Excel view

--- django_tablib/views.py
+++ django_tablib/views.py
@@ -1,4 +1,14 @@
+from django.http import HttpResponse
+
 from core import Dataset
 
 def export(request, queryset=None, model=None, headers=None):
-    pass
+    if queryset is None:
+        queryset = model.objects.all()
+
+    dataset = Dataset(queryset, headers=headers)
+    filename = 'export.xls'
+    response = HttpResponse(dataset.xls, mimetype='application/vnd.ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return response
+    
