f0a730ec4e5d9c40909ffeb5f5e4c7a07ea6bace
Josh Ourisman <josh@joshourisman.com>
2010-10-20 14:36:05 -0400

    making sure to set header_list AND attr_list if no headers passed

--- django_tablib/core.py
+++ django_tablib/core.py
@@ -8,7 +8,8 @@ class Dataset(tablib.Dataset):
     def __init__(self, queryset, headers=None):
         if headers is None:
             fields = queryset.model._meta.fields
-            self.attr_list = [field.name for field in fields]
+            self.header_list = [field.name for field in fields]
+            self.attr_list = self.header_list
         elif type(headers) is dict:
             self.header_dict = headers
             self.header_list = self.header_dict.keys()
