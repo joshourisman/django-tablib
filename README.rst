eda07e101d60c66d5e2204dea55333e1f7d1af3c
Chris Adams <chris@improbable.org>
2011-01-31 10:56:34 -0500

    Basic generic export function
    This allows you to export many models using a settings dictionary

--- README.rst
+++ README.rst
@@ -30,7 +30,7 @@ Create a tablib Dataset from a Django QuerySet with a dictionary mapping custom 
 
     data = django_tablib.Dataset(MyModel.objects.all(), headers = {
         'Awesome Descriptive Column Header': 'boring_field_name',
-	})
+    })
 
 Add a new row: ::
 
@@ -49,8 +49,28 @@ For everything else see the tablib documentation!
 Django Integration
 ------------------
 
-django_tablib now provides a generic Django view to automatically export your querysets to an Excel spreadsheet. In your urls.py: ::
+django_tablib now provides a generic Django view to automatically export your
+querysets to an Excel spreadsheet. In your urls.py::
 
     (r'^export/$', 'django_tablib.views.export', {
         'model': MyModel,
-	})
+    })
+
+If you have many models to export you may prefer use the generic export view:
+
+#. Add the view to ``urlpatterns`` in ``urls.py``::
+
+    url(r'export/(?P<model_name>[^/]+)/$', "django_tablib.views.generic_export"),
+
+#. Create the ``settings.TABLIB_MODELS`` dictionary using lower-case model
+   names in "app.model" format as keys and the permitted `field lookups
+   <http://docs.djangoproject.com/en/dev/ref/models/querysets/#field-lookups>`_
+   or ``None`` as values::
+
+       TABLIB_MODELS = {
+           'myapp.simple': None,
+           'myapp.related': {'simple__title': ('exact', 'iexact')},
+       }
+
+#. Open ``/export/myapp.simple`` or
+   ``/export/myapp.related/?simple__title__iexact=test``
--- django_tablib/views.py
+++ django_tablib/views.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
 
-from django.http import HttpResponse
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.http import Http404, HttpResponse, HttpResponseBadRequest
+from django.db.models.loading import get_model
 
 from .core import Dataset
+
 
 def export(request, queryset=None, model=None, headers=None):
     if queryset is None:
@@ -13,4 +17,60 @@ def export(request, queryset=None, model=None, headers=None):
     response = HttpResponse(dataset.xls, mimetype='application/vnd.ms-excel')
     response['Content-Disposition'] = 'attachment; filename=%s' % filename
     return response
-    
+
+
+def generic_export(request, model_name=None):
+    """
+    Generic view configured through settings.TABLIB_MODELS
+
+    Usage:
+        1. Add the view to ``urlpatterns`` in ``urls.py``::
+            url(r'export/(?P<model_name>[^/]+)/$', "django_tablib.views.generic_export"),
+        2. Create the ``settings.TABLIB_MODELS`` dictionary using model names
+           as keys the allowed lookup operators as values, if any::
+
+           TABLIB_MODELS = {
+               'myapp.simple': None,
+               'myapp.related': {'simple__title': ('exact', 'iexact')},
+           }
+        3. Open ``/export/myapp.simple`` or
+           ``/export/myapp.related/?simple__title__iexact=test``
+    """
+
+    if model_name not in settings.TABLIB_MODELS:
+        raise Http404()
+
+    model = get_model(*model_name.split(".", 2))
+    if not model:
+        raise ImproperlyConfigured("Model %s is in settings.TABLIB_MODELS but"
+                                    " could not be loaded" % model_name)
+
+    qs = model._default_manager.all()
+
+    # Filtering may be allowed based on TABLIB_MODELS:
+    filter_settings = settings.TABLIB_MODELS[model_name]
+    filters = {}
+
+    for k, v in request.GET.items():
+        try:
+            # Allow joins (they'll be checked below) but chop off the trailing
+            # lookup operator:
+            rel, lookup_type = k.rsplit("__", 1)
+        except ValueError:
+            rel = k
+            lookup_type = "exact"
+
+        allowed_lookups = filter_settings.get(rel, None)
+
+        if allowed_lookups is None:
+            return HttpResponseBadRequest("Filtering on %s is not allowed" % rel)
+        elif lookup_type not in allowed_lookups:
+            return HttpResponseBadRequest("%s may only be filtered using %s"
+                                            % (k, " ".join(allowed_lookups)))
+        else:
+            filters[str(k)] = v
+
+    if filters:
+        qs = qs.filter(**filters)
+
+    return export(request, model=model, queryset=qs)
