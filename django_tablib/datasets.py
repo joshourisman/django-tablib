fff8ab4c3a75fdf2bf3ebcc79194305f66053ae9
Josh Ourisman <josh@joshourisman.com>
2011-02-04 07:41:21 -0500

    brought back old-style Dataset in datasets.py for use in things like views

---
+++ django_tablib/datasets.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+from .base import BaseDataset
+
+class Dataset(BaseDataset):
+    def __init__(self, queryset, headers=None):
+        self.queryset = queryset
+        if headers is None:
+            fields = queryset.model._meta.fields
+            self.header_list = [field.name for field in fields]
+            self.attr_list = self.header_list
+        elif type(headers) is dict:
+            self.header_dict = headers
+            self.header_list = self.header_dict.keys()
+            self.attr_list = self.header_dict.values()
+        elif type(headers) is list:
+            self.header_list = headers
+            self.attr_list = headers
+        super(Dataset, self).__init__()
+        
