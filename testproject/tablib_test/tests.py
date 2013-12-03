ccff02961493321956875ddd7ba51db344b007a5
Josh Ourisman <josh@joshourisman.com>
2013-12-03 12:02:30 -0500

    Testing that we can successfully set fields.

--- testproject/tablib_test/tests.py
+++ testproject/tablib_test/tests.py
@@ -16,3 +16,16 @@ class DjangoTablibTestCase(TestCase):
         self.assertEqual(len(data.headers), 2)
         self.assertTrue('id' in data.headers)
         self.assertTrue('field1' in data.headers)
+
+    def test_fields(self):
+        class TestModelDataset(ModelDataset):
+            fields = ['field1']
+
+            class Meta:
+                model = TestModel
+
+        data = TestModelDataset()
+
+        self.assertEqual(len(data.headers), 1)
+        self.assertTrue('id' not in data.headers)
+        self.assertTrue('field1' in data.headers)
