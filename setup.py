9c4f24d9cc4e14f1ff8da62800da2a9bab0d97b4
Josh Ourisman <josh@joshourisman.com>
2013-12-03 11:14:18 -0500

    Upped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 description = "A wrapper around Kenneth Reitz' tablib to work with Django models."
 
-VERSION = '2.5'
+VERSION = '2.6'
 
 setup(
     name='django-tablib',
