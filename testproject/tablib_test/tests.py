2191dbf10b43890bb751aad054789926d8660031
Josh Ourisman <josh@joshourisman.com>
2013-12-03 12:27:32 -0500

    Added test for declarative fields.

--- testproject/tablib_test/tests.py
+++ testproject/tablib_test/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from django_tablib import ModelDataset
+from django_tablib import ModelDataset, Field
 
 from .models import TestModel
 
@@ -44,3 +44,16 @@ class DjangoTablibTestCase(TestCase):
         self.assertTrue('id' not in data.headers)
         self.assertTrue('field1' not in data.headers)
         self.assertTrue('Field 1' in data.headers)
+
+    def test_declarative_fields(self):
+        class TestModelDataset(ModelDataset):
+            field1 = Field()
+
+            class Meta:
+                model = TestModel
+
+        data = TestModelDataset()
+
+        self.assertEqual(len(data.headers), 1)
+        self.assertTrue('id' not in data.headers)
+        self.assertTrue('field1' in data.headers)
